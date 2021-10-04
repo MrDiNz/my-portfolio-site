7442cb599bc35b2fa57c31e1f50eb907edf0d911
MrDiNz <poo.hu@hotmail.com>
2021-10-04 21:10:43 +0700

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Button, Image} from "semantic-ui-react";
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Button>asdsad</Button>
+        <Button>asdsad</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
